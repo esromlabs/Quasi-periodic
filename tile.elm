d1c13c0e60a13ee8225df4a301c5162534a90052
Nate Morse <morse.nate@gmail.com>
2015-11-22 14:21:17 -0500

    Update tile.elm

--- tile.elm
+++ tile.elm
@@ -6,7 +6,7 @@ import Graphics.Element exposing (..)
 main : Element
 main =
   collage 300 300
-    (gen 19 "f" 0.0 0.0 90.0 100.0)
+    (gen 3 "f" 0.0 0.0 90.0 100.0)
 
 
 clearGrey : Color
@@ -15,11 +15,17 @@ clearGrey =
   
 gen: Int -> String -> Float -> Float -> Float -> Float -> List (Form)
 gen level kind x y rot scale =
-  if level > 0 then
-    (pentagon x y rot scale :: 
-      gen (level-1) "f" (x+scale) (y+scale) (rot+72.0) (scale * -0.6018))
+  if level == 0 then
+    ([pentagon x y rot scale])
   else
-    ([])
+    if kind == "f" then
+      (pentagon x y rot scale :: 
+        List.append (gen (level-1) "f" (x+scale) (y+scale) (rot+36.0) (scale * -0.6018))
+        (gen (level-1) "f" (x+scale) (y+scale) (rot+72.0) (scale * -0.6018)))
+    else
+      (pentagon x y rot scale :: 
+        gen (level-1) "f" (x+scale) (y+scale) (rot+72.0) (scale * -0.6018))
+      
     
 pentagon : Float -> Float -> Float ->  Float -> Form
 pentagon x y r s = 
